b259c792f25e87d22e6199e08872b419074fb48a
Ocean <hophuong.ocean@gmail.com>
2024-03-11 04:35:36 +0000

    refactored unit tests to use fixtures

--- test.py
+++ test.py
@@ -2,24 +2,21 @@ import pytest
 
 import main
 
-def test_create_queue():
-    x = main.Queue(cap=3)
-    assert x.queue == [0, 0, 0]
-    assert x.front == -1
-    assert x.rear == -1
-    assert x.cap == 3
-
-
-def test_full_queue():
-    x = main.Queue(cap=3)
-    x.rear = 3
-    assert main.Queue.is_full(x) is True
-
-
 # Create fixture for empty queue
 @pytest.fixture
 def init_queue():
     return main.Queue(cap=3)
+
+def test_create_queue(init_queue):
+    assert init_queue.queue == [0, 0, 0]
+    assert init_queue.front == -1
+    assert init_queue.rear == -1
+    assert init_queue.cap == 3
+
+
+def test_full_queue(init_queue):
+    init_queue.rear = 3
+    assert main.Queue.is_full(init_queue) is True
 
 
 def test_enqueue_initialised_queue(init_queue):
@@ -27,4 +24,3 @@ def test_enqueue_initialised_queue(init_queue):
     assert init_queue.front == 0
     assert init_queue.rear == 0
     assert init_queue.queue == [1, 0, 0]
-
