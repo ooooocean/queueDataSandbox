28b4d2b0f632a3cf481a8e84495d082eacbdfa27
Ocean <hophuong.ocean@gmail.com>
2024-03-11 04:48:45 +0000

    refactored unit tests for empty queues

--- test.py
+++ test.py
@@ -4,23 +4,37 @@ import main
 
 # Create fixture for empty queue
 @pytest.fixture
-def init_queue():
+def empty_queue():
     return main.Queue(cap=3)
 
-def test_create_queue(init_queue):
-    assert init_queue.queue == [0, 0, 0]
-    assert init_queue.front == -1
-    assert init_queue.rear == -1
-    assert init_queue.cap == 3
+
+def non_empty_queue():
+    x = main.Queue(cap=3)
+    x.enqueue(1)
+    return x
 
 
-def test_full_queue(init_queue):
-    init_queue.rear = 3
-    assert main.Queue.is_full(init_queue) is True
+def test_create_queue(empty_queue):
+    assert empty_queue.queue == [0, 0, 0]
+    assert empty_queue.front == -1
+    assert empty_queue.rear == -1
+    assert empty_queue.cap == 3
 
 
-def test_enqueue_initialised_queue(init_queue):
-    main.Queue.enqueue(init_queue, 1)
-    assert init_queue.front == 0
-    assert init_queue.rear == 0
-    assert init_queue.queue == [1, 0, 0]
+def test_full_queue_is_full(empty_queue):
+    empty_queue.rear = 3
+    assert main.Queue.is_full(empty_queue) is True
+
+
+def test_empty_queue_is_empty(empty_queue):
+    assert main.Queue.is_empty(empty_queue) is True
+
+
+def test_empty_queue_is_not_full(empty_queue):
+    assert main.Queue.is_full(empty_queue) is False
+
+def test_enqueue_initialised_queue(empty_queue):
+    main.Queue.enqueue(empty_queue, 1)
+    assert empty_queue.front == 0
+    assert empty_queue.rear == 0
+    assert empty_queue.queue == [1, 0, 0]
